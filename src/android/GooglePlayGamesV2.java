8d88c4a987e5b38caa1613ed4959da848f064fe6
Nascorpent <nascorpentertainment@gmail.com>
2024-09-02 14:34:42 -0300

    6

--- src/android/GooglePlayGamesV2.java
+++ src/android/GooglePlayGamesV2.java
@@ -13,8 +13,6 @@ import com.google.android.gms.games.PlayGamesSdk;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaWebView;
 
-import android.content.Intent;
-
 import com.google.android.gms.games.SnapshotsClient;
 import com.google.android.gms.games.snapshot.Snapshot;
 import com.google.android.gms.games.snapshot.SnapshotMetadata;
@@ -25,6 +23,7 @@ import com.google.android.gms.tasks.Task;
 import org.json.JSONObject;
 import android.graphics.BitmapFactory;
 import android.util.Base64;
+import androidx.annotation.NonNull;
 
 public class GooglePlayGamesV2 extends CordovaPlugin {
 
@@ -32,8 +31,8 @@ public class GooglePlayGamesV2 extends CordovaPlugin {
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView appView) {
-        super.initialize(cordova, appView);if (!playGamesInitialized) {
-
+        super.initialize(cordova, appView);
+        if (!playGamesInitialized) {
             PlayGamesSdk.initialize(cordova.getActivity());
             playGamesInitialized = true;
         }
@@ -41,43 +40,48 @@ public class GooglePlayGamesV2 extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("signInSilently")) {
-            this.signInSilently(callbackContext);
-            return true;
-        } else if (action.equals("signIn")) {
-            this.signIn(callbackContext);
-            return true;
-        } else if (action.equals("signOut")) {
-            this.signOut(callbackContext);
-            return true;
-        } else if (action.equals("saveGame")) {
-            this.saveGame(args, callbackContext);
-            return true;
-        } else if (action.equals("loadGame")) {
-            this.loadGame(args, callbackContext);
-            return true;
+        switch (action){
+            case "signInSilently":
+                this.signInSilently(callbackContext);
+                return true;
+            case "signIn":
+                this.signIn(callbackContext);
+                return true;
+            case "signOut":
+                this.signOut(callbackContext);
+                return true;
+            case "saveGame":
+                this.saveGame(args, callbackContext);
+                return true;
+            case "loadGame":
+                this.loadGame(args, callbackContext);
+                return true;
+            default:
+                return false;
         }
-        return false;
     }
 
     private void signInSilently(CallbackContext callbackContext) {
         GamesSignInClient gamesSignInClient = PlayGames.getGamesSignInClient(cordova.getActivity());
-    
+
         gamesSignInClient.isAuthenticated().addOnCompleteListener(isAuthenticatedTask -> {
-            boolean isAuthenticated = (isAuthenticatedTask.isSuccessful() &&
-                    isAuthenticatedTask.getResult().isAuthenticated());
-    
-            if (isAuthenticated) {
-                PlayGames.getPlayersClient(cordova.getActivity()).getCurrentPlayer()
-                        .addOnCompleteListener(playerTask -> {
-                            if (playerTask.isSuccessful()) {
-                                String playerId = playerTask.getResult().getPlayerId();
-                                callbackContext.success(playerId);
-                            } else {
-                                callbackContext.error("signInSilently:failed to get player ID");
-                            }
-                        });
-            } else{
+            if (isAuthenticatedTask.isSuccessful()) {
+                boolean isAuthenticated = isAuthenticatedTask.getResult().isAuthenticated();
+
+                if (isAuthenticated) {
+                    PlayGames.getPlayersClient(cordova.getActivity()).getCurrentPlayer()
+                            .addOnCompleteListener(playerTask -> {
+                                if (playerTask.isSuccessful()) {
+                                    String playerId = playerTask.getResult().getPlayerId();
+                                    callbackContext.success(playerId);
+                                } else {
+                                    callbackContext.error("signInSilently:failed to get player ID");
+                                }
+                            });
+                } else {
+                    callbackContext.error("signInSilently:failed");
+                }
+            } else {
                 callbackContext.error("signInSilently:failed");
             }
         });
@@ -86,30 +90,25 @@ public class GooglePlayGamesV2 extends CordovaPlugin {
     private void signIn(CallbackContext callbackContext) {
         GamesSignInClient gamesSignInClient = PlayGames.getGamesSignInClient(cordova.getActivity());
         gamesSignInClient.signIn().addOnCompleteListener(signInTask -> {
-          if (signInTask.isSuccessful()) {
-            PlayGames.getPlayersClient(cordova.getActivity()).getCurrentPlayer()
-                    .addOnCompleteListener(playerTask -> {
-                      if (playerTask.isSuccessful()) {
-                        String playerId = playerTask.getResult().getPlayerId();
-                        callbackContext.success(playerId);
-                      } else {callbackContext.error("signIn:failed to get player ID");
-                      }
-                    });
-          } else {
-            callbackContext.error("signIn:failed");
-          }
-        });
-      }
-
-    private void signOut(CallbackContext callbackContext) {
-        GamesSignInClient gamesSignInClient = PlayGames.getGamesSignInClient(cordova.getActivity());
-        gamesSignInClient.signOut().addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                callbackContext.success();
+            if (signInTask.isSuccessful()) {
+                PlayGames.getPlayersClient(cordova.getActivity()).getCurrentPlayer()
+                        .addOnCompleteListener(playerTask -> {
+                            if (playerTask.isSuccessful()) {
+                                String playerId = playerTask.getResult().getPlayerId();
+                                callbackContext.success(playerId);
+                            } else {
+                                callbackContext.error("signIn:failed to get player ID");
+                            }
+                        });
             } else {
-                callbackContext.error("signOut:failed");
+                callbackContext.error("signIn:failed");
             }
         });
+    }
+
+    private void signOut(CallbackContext callbackContext) {
+        PlayGames.signOut(cordova.getActivity()); // Correção: signOut agora é um método estático de PlayGames
+        callbackContext.success(); // Chame success após signOut
     }
 
     private void saveGame(JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -118,104 +117,86 @@ public class GooglePlayGamesV2 extends CordovaPlugin {
         String description = args.getString(3);
         long timestamp = args.getLong(4);
     
-        SnapshotsClient snapshotsClient = Games.getSnapshotsClient(cordova.getActivity());
+        SnapshotsClient snapshotsClient = PlayGames.getSnapshotsClient(cordova.getActivity());
     
-        snapshotsClient.open(snapshotName, true).continueWith(new Continuation<SnapshotsClient.DataOrConflict<Snapshot>, Task<SnapshotMetadata>>() {
-            @Override
-            public Task<SnapshotMetadata> then(@NonNull Task<SnapshotsClient.DataOrConflict<Snapshot>> task) throws Exception {
-                SnapshotsClient.DataOrConflict<Snapshot> result = task.getResult();
+        snapshotsClient.open(snapshotName, true, SnapshotsClient.RESOLUTION_POLICY_MOST_RECENTLY_MODIFIED)
+                .continueWith(new Continuation<SnapshotsClient.DataOrConflict<Snapshot>, Task<SnapshotMetadata>>() {
+                    @Override
+                    public Task<SnapshotMetadata> then(@NonNull Task<SnapshotsClient.DataOrConflict<Snapshot>> task) throws Exception {
+                        SnapshotsClient.DataOrConflict<Snapshot> result = task.getResult();
     
-                if (result.isConflict()) {
-                    // Descartar o snapshot conflitante
-                    Snapshot conflictSnapshot = result.getConflict();
-                    snapshotsClient.discardAndClose(conflictSnapshot);
+                        Snapshot snapshot = result.getData();
+                        snapshot.getSnapshotContents().writeBytes(data.getBytes());
     
-                    // Abrir o snapshot novamente (sem criar um novo)
-                    return snapshotsClient.open(snapshotName, false).continueWith(new Continuation<SnapshotsClient.DataOrConflict<Snapshot>, Task<SnapshotMetadata>>() {
-                        @Override
-                        public Task<SnapshotMetadata> then(@NonNull Task<SnapshotsClient.DataOrConflict<Snapshot>> task) throws Exception {
-                            Snapshot snapshot = task.getResult().getData();
-                            snapshot.getSnapshotContents().writeBytes(data.getBytes());
-    
-                            SnapshotMetadataChange metadataChange = new SnapshotMetadataChange.Builder()
-                                    .fromMetadata(snapshot.getMetadata())
-                                    .setDescription(description)
-                                    .setPlayedTimeMillis(timestamp)
-                                    .build();
-    
-                            if (!coverImage.isEmpty()) {
-                                byte[] imageBytes = Base64.decode(coverImage, Base64.DEFAULT);
-                                metadataChange = new SnapshotMetadataChange.Builder()
-                                        .fromMetadataChange(metadataChange)
-                                        .setCoverImage(BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length))
-                                        .build();
-                            }
-    
-                            return snapshotsClient.commitAndClose(snapshot, metadataChange);
-                        }
-                    });
-                } else {
-                    Snapshot snapshot = result.getData();
-                    snapshot.getSnapshotContents().writeBytes(data.getBytes());
-    
-                    SnapshotMetadataChange metadataChange = new SnapshotMetadataChange.Builder()
-                            .fromMetadata(snapshot.getMetadata())
-                            .setDescription(description)
-                            .setPlayedTimeMillis(timestamp)
-                            .build();
-    
-                    if (!coverImage.isEmpty()) {
-                        byte[] imageBytes = Base64.decode(coverImage, Base64.DEFAULT);
-                        metadataChange = new SnapshotMetadataChange.Builder()
-                                .fromMetadataChange(metadataChange)
-                                .setCoverImage(BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length))
+                        SnapshotMetadataChange metadataChange = new SnapshotMetadataChange.Builder()
+                                .fromMetadata(snapshot.getMetadata())
+                                .setDescription(description)
+                                .setPlayedTimeMillis(timestamp)
                                 .build();
-                    }
     
-                    return snapshotsClient.commitAndClose(snapshot, metadataChange);
-                }
-            }
-        }).addOnCompleteListener(new OnCompleteListener<SnapshotMetadata>() {
-            @Override
-            public void onComplete(@NonNull Task<SnapshotMetadata> task) {
-                if (task.isSuccessful()) {
-                    callbackContext.success();
-                } else {
-                    callbackContext.error("saveGame:failed");
-                }
-            }
-        });
+                        if (!coverImage.isEmpty()) {
+                            byte[] imageBytes = Base64.decode(coverImage, Base64.DEFAULT);
+                            metadataChange = new SnapshotMetadataChange.Builder()
+                                    .fromMetadata(snapshot.getMetadata())
+                                    .setCoverImage(BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length))
+                                    .build();
+                        }
+    
+                        return snapshotsClient.commitAndClose(snapshot, metadataChange);
+                    }
+                })
+                .addOnCompleteListener(new OnCompleteListener<Task<SnapshotMetadata>>() { // Alterar o tipo aqui
+                    @Override
+                    public void onComplete(@NonNull Task<SnapshotMetadata> task) {
+                        if (task.isSuccessful()) {
+                            callbackContext.success();
+                        } else {
+                            callbackContext.error("saveGame:failed");
+                        }
+                    }
+                });
     }
 
     private void loadGame(JSONArray args, CallbackContext callbackContext) throws JSONException {
         String snapshotName = args.getString(0);
     
-        SnapshotsClient snapshotsClient = Games.getSnapshotsClient(cordova.getActivity());
+        SnapshotsClient snapshotsClient = PlayGames.getSnapshotsClient(cordova.getActivity());
     
-        snapshotsClient.open(snapshotName, false).continueWith(new Continuation<SnapshotsClient.DataOrConflict<Snapshot>, Task<byte[]>>() {
-            @Override
-            public Task<byte[]> then(@NonNull Task<SnapshotsClient.DataOrConflict<Snapshot>> task) throws Exception {
-                Snapshot snapshot = task.getResult().getData();
-                return snapshotsClient.loadSnapshotContents(snapshot);
-            }
-        }).addOnCompleteListener(new OnCompleteListener<byte[]>() {
-            @Override
-            public void onComplete(@NonNull Task<byte[]> task) {
-                if (task.isSuccessful()) {
-                    String data = new String(task.getResult());
-                    try {
-                        JSONObject json = new JSONObject();
-                        json.put("data", data);
-                        // Inclua outros metadados, se necessário
-                        // json.put("timestamp", snapshot.getMetadata().getLastModifiedTimestamp()); 
-                        callbackContext.success(json);
-                    } catch (JSONException e) {
-                        callbackContext.error("loadGame:failed to parse data");
+        snapshotsClient.open(snapshotName, false, 0)
+                .continueWith(new Continuation<SnapshotsClient.DataOrConflict<Snapshot>, Task<Snapshot>>() {
+                    @Override
+                    public Task<Snapshot> then(@NonNull Task<SnapshotsClient.DataOrConflict<Snapshot>> task) throws Exception {
+                        Snapshot snapshot = task.getResult().getData();
+                        return snapshotsClient.load(snapshot.getMetadata()); // Passar SnapshotMetadata para load
                     }
-                } else {
-                    callbackContext.error("loadGame:failed");
-                }
-            }
-        });
+                })
+                .continueWith(new Continuation<Snapshot, byte[]>() { // Adicionar continueWith para obter bytes
+                    @Override
+                    public byte[] then(@NonNull Task<Snapshot> task) throws Exception {
+                        return task.getResult().getSnapshotContents().readFully();
+                    }
+                })
+                .addOnCompleteListener(new OnCompleteListener<byte[]>() {
+                    @Override
+                    public void onComplete(@NonNull Task<byte[]> task) {
+                        if (task.isSuccessful()) {
+                            try {
+                                byte[] data = task.getResult();
+                                if (data != null) {
+                                    String dataString = new String(data);
+                                    JSONObject json = new JSONObject();
+                                    json.put("data", dataString);
+                                    callbackContext.success(json);
+                                } else{
+                                    callbackContext.error("loadGame:failed - no data found");
+                                }
+                            } catch (JSONException e) {
+                                callbackContext.error("loadGame:failed to parse data - " + e.getMessage());
+                            }
+                        } else {
+                            callbackContext.error("loadGame:failed");
+                        }
+                    }
+                });
     }
 }
